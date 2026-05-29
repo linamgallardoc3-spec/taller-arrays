5aed83d831d220dd64669509999ea25a7a007109
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-05-29 11:34:19 -0500

    Taller arrays

---
+++ README.md
@@ -0,0 +1,9 @@
+Taller:
+
+Variables y tipos de datos.
+Funciones.
+Condicionales (if/else).
+Bucles (for, while).
+Arrays y métodos básicos.
+
+Lina Gallardo Corrales
---
+++ taller-arrays.js
@@ -0,0 +1,7 @@
+/*
+    Ejercicio 1: Calculadora de Propinas
+    - Una app para calcular propinas en restaurantes.
+    Crea una función calcularPropina(total, porcentaje) que reciba el total de la cuenta y el porcentaje de
+    propina, y retorne el monto final a pagar (total + propina)
+*/
+
